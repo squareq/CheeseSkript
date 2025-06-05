e2d8a360038e89117959dbde768d3bab2c6a2192
MrErrors <111009025+Mr-Errors@users.noreply.github.com>
2025-06-05 10:18:43 -0400

    Add Option to Print Errors in Reload Script Effect (#7830)
    * init commit
    
    * switched to printing instead of catching errors

--- src/main/java/ch/njol/skript/effects/EffScriptFile.java
+++ src/main/java/ch/njol/skript/effects/EffScriptFile.java
@@ -2,13 +2,26 @@ package ch.njol.skript.effects;
 
 import ch.njol.skript.ScriptLoader;
 import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptCommand;
+import ch.njol.skript.command.ScriptCommand;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
+import ch.njol.skript.localization.Language;
+import ch.njol.skript.localization.PluralizingArgsMessage;
+import ch.njol.skript.log.LogEntry;
+import ch.njol.skript.log.LogHandler;
+import ch.njol.skript.log.RedirectingLogHandler;
+import ch.njol.skript.log.RetainingLogHandler;
+import ch.njol.skript.log.TimingLogHandler;
 import ch.njol.skript.registrations.Feature;
+import ch.njol.skript.util.Utils;
+import ch.njol.util.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 import org.skriptlang.skript.lang.script.Script;
@@ -21,7 +34,10 @@ import org.bukkit.event.Event;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
 
 @Name("Enable/Disable/Unload/Reload Script")
 @Description("""
@@ -41,9 +57,9 @@ public class EffScriptFile extends Effect {
 
 	static {
 		Skript.registerEffect(EffScriptFile.class,
-			"(1:(enable|load)|2:reload|3:disable|4:unload) script [file|named] %string%",
-			"(1:(enable|load)|2:reload|3:disable|4:unload) skript file %string%",
-			"(1:(enable|load)|2:reload|3:disable|4:unload) %scripts%"
+			"(1:(enable|load)|2:reload|3:disable|4:unload) script [file|named] %string% [print:with errors]",
+			"(1:(enable|load)|2:reload|3:disable|4:unload) skript file %string% [print:with errors]",
+			"(1:(enable|load)|2:reload|3:disable|4:unload) %scripts% [print:with errors]"
 		);
 		/*
 			The string-pattern must come first (since otherwise `script X` would match the expression)
@@ -57,12 +73,13 @@ public class EffScriptFile extends Effect {
 
 	private @UnknownNullability Expression<String> scriptNameExpression;
 	private @UnknownNullability Expression<Script> scriptExpression;
-	private boolean scripts, hasReflection;
+	private boolean scripts, hasReflection, printErrors;
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
 		this.mark = parseResult.mark;
+		printErrors = parseResult.hasTag("print");
 		switch (matchedPattern) {
 			case 0, 1:
 				this.scriptNameExpression = (Expression<String>) exprs[0];
@@ -77,20 +94,28 @@ public class EffScriptFile extends Effect {
 
 	@Override
 	protected void execute(Event event) {
+		Set<CommandSender> recipients = new HashSet<>();
+		if (printErrors) {
+			recipients.addAll(Bukkit.getOnlinePlayers().stream()
+				.filter(player -> player.hasPermission("skript.reloadnotify"))
+				.collect(Collectors.toSet()));
+		}
+		RedirectingLogHandler logHandler = new RedirectingLogHandler(recipients, "").start();
+
 		if (scripts) {
 			for (Script script : scriptExpression.getArray(event)) {
 				@Nullable File file = script.getConfig().getFile();
-				this.handle(file, script.getConfig().getFileName());
+				this.handle(file, script.getConfig().getFileName(), logHandler);
 			}
 		} else {
 			String name = scriptNameExpression.getSingle(event);
-			if (name == null)
-				return;
-			this.handle(ScriptLoader.getScriptFromName(name), name);
+			if (name != null)
+				this.handle(ScriptLoader.getScriptFromName(name), name, logHandler);
 		}
+		logHandler.close();
 	}
 
-	private void handle(@Nullable File scriptFile, @Nullable String name) {
+	private void handle(@Nullable File scriptFile, @Nullable String name, OpenCloseable openCloseable) {
 		if (scriptFile == null || !scriptFile.exists())
 			return;
 		if (name == null)
@@ -117,7 +142,7 @@ public class EffScriptFile extends Effect {
 					}
 				}
 
-				ScriptLoader.loadScripts(scriptFile, OpenCloseable.EMPTY);
+				ScriptLoader.loadScripts(scriptFile, openCloseable);
 				break;
 			case RELOAD:
 				if (filter.accept(scriptFile))
@@ -125,7 +150,7 @@ public class EffScriptFile extends Effect {
 
 				this.unloadScripts(scriptFile);
 
-				ScriptLoader.loadScripts(scriptFile, OpenCloseable.EMPTY);
+				ScriptLoader.loadScripts(scriptFile, openCloseable);
 				break;
 			case UNLOAD:
 				if (hasReflection) { // if we don't use the new features this falls through into DISABLE
