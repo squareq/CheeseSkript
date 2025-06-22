84772b6132d9ade8f9f0079cac4c1e378e437c85
Chick Chicky <91527355+ChickChicky@users.noreply.github.com>
2025-06-22 22:24:54 +0200

    Fix event-itemstack for ComplexRecipe (#7674)
    * Fix event-itemstack for ComplexRecipe
    
    * Fix inventoryClickedEvent with complexRecipe events
    
    * Fix indendation of line 156 of EvtItem.java

--- src/main/java/ch/njol/skript/events/EvtItem.java
+++ src/main/java/ch/njol/skript/events/EvtItem.java
@@ -15,6 +15,7 @@ import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.ComplexRecipe;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.jetbrains.annotations.Nullable;
@@ -161,7 +162,12 @@ public class EvtItem extends SkriptEvent {
 		} else if (event instanceof EntityDropItemEvent entityDropItemEvent) {
 			itemStack = entityDropItemEvent.getItemDrop().getItemStack();
 		} else if (event instanceof CraftItemEvent craftItemEvent) {
-			itemStack = craftItemEvent.getRecipe().getResult();
+			Recipe recipe = craftItemEvent.getRecipe();
+			if (recipe instanceof ComplexRecipe) {
+				itemStack = craftItemEvent.getCurrentItem();
+			} else {
+				itemStack = recipe.getResult();
+			}
 		} else if (event instanceof PrepareItemCraftEvent prepareItemCraftEvent) {
 			Recipe recipe = prepareItemCraftEvent.getRecipe();
 			if (recipe != null) {
