98d58461b83fb126c10bac8b5ecaf87910b1ad60
Patrick Miller <apickledwalrus@icloud.com>
2025-07-30 15:46:11 -0400

    Improve default syntax ordering (#8076)

--- src/main/java/org/skriptlang/skript/registration/SyntaxRegister.java
+++ src/main/java/org/skriptlang/skript/registration/SyntaxRegister.java
@@ -14,13 +14,45 @@ import java.util.concurrent.ConcurrentSkipListSet;
  */
 final class SyntaxRegister<I extends SyntaxInfo<?>> {
 
+	private static int calculateComplexityScore(SyntaxInfo<?> info) {
+		return info.patterns().stream()
+				.mapToInt(SyntaxRegister::calculateComplexityScore)
+				.max()
+				.orElseThrow(); // a syntax info should have at least one pattern
+	}
+
+	private static int calculateComplexityScore(String pattern) {
+		int score = 0;
+		char[] chars = pattern.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i] == '%') {
+				// weigh "%thing% %thing%" or "%thing% [%thing%]" heavier
+				if ((i - 2 >= 0 && chars[i - 2] == '%') || (i - 3 >= 0 && chars[i - 3] == '%')) {
+					score += 3;
+				} else {
+					score++;
+				}
+			}
+		}
+		return score;
+	}
+
 	private static final Comparator<SyntaxInfo<?>> SET_COMPARATOR = (a,b) -> {
 		if (a == b) { // only considered equal if registering the same infos
 			return 0;
 		}
-		int result = a.priority().compareTo(b.priority());
-		// when elements have the same priority, order by hashcode
-		return result != 0 ? result : Integer.compare(a.hashCode(), b.hashCode());
+		// priority is the primary factor in determining ordering
+		int priorityResult = a.priority().compareTo(b.priority());
+		if (priorityResult != 0) {
+			return priorityResult;
+		}
+		// otherwise, consider the complexity of the syntax
+		int scoreResult = Integer.compare(calculateComplexityScore(a), calculateComplexityScore(b));
+		if (scoreResult != 0) {
+			return scoreResult;
+		}
+		// otherwise, order by hashcode
+		return Integer.compare(a.hashCode(), b.hashCode());
 	};
 
 	final Set<I> syntaxes = new ConcurrentSkipListSet<>(SET_COMPARATOR);
