8f657a81ae94bb86568c081d3eb8535148210a2d
SirSmurfy2 <82696841+Absolutionism@users.noreply.github.com>
2025-09-30 19:51:22 -0400

    Equippable components (#7194)
    * Starter Commit
    
    * PR ready
    
    * remove "runtime"
    
    * Changes
    
    * Update
    
    * Update
    
    * Overhaul
    
    * Update CondEquipCompInteract.java
    
    * Requested Changes
    
    * Partial Changes
    
    * Update EquippableComponents.sk
    
    * revert
    
    * Record Usage
    
    * Partial Changes
    
    * More Changes
    
    * Changes
    
    * Requested Changes
    
    * Update EquippableComponents.sk
    
    * Update EquippableComponents.sk
    
    * Adooblus Changes
    
    * Update EquippableExperiment.java
    
    * Update Experiment Class  Name
    
    * Requested Changes
    
    * Cloneable Changes
    
    * Update EquippableModule.java
    
    * Update ItemComponentModule.java
    
    * Partial Update
    
    Experiencing some issues, need to see if incorrect merge
    
    * Docs Update
    
    * Missing Import
    
    * More Elements
    
    * Docs Fix
    
    * Fix ItemType
    
    * Revert Misc Changed Files
    
    * Source Retainability
    
    * Update EquippableComponents.sk
    
    * Update ExprEquippableComponent.java
    
    * Requested Changes
    
    * Update Wrapper
    
    * ComponentUtils
    
    * Docs Update
    
    * Update ExprSecBlankEquipComp.java
    
    * Merge dev/feature
    
    * Requested Changes
    
    * Update src/main/java/org/skriptlang/skript/bukkit/itemcomponents/equippable/elements/ExprEquipCompModel.java
    
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>
    
    * Pickles Changes - Part 20
    
    * Update CondEquipCompShearable.java

--- src/main/java/ch/njol/skript/Skript.java
+++ src/main/java/ch/njol/skript/Skript.java
@@ -98,6 +98,7 @@ import org.skriptlang.skript.bukkit.fishing.FishingModule;
 import org.skriptlang.skript.bukkit.furnace.FurnaceModule;
 import org.skriptlang.skript.bukkit.input.InputModule;
 import org.skriptlang.skript.bukkit.log.runtime.BukkitRuntimeErrorConsumer;
+import org.skriptlang.skript.bukkit.itemcomponents.ItemComponentModule;
 import org.skriptlang.skript.bukkit.loottables.LootTableModule;
 import org.skriptlang.skript.bukkit.registration.BukkitRegistryKeys;
 import org.skriptlang.skript.bukkit.registration.BukkitSyntaxInfos;
@@ -587,7 +588,7 @@ public final class Skript extends JavaPlugin implements Listener {
 			TagModule.load();
 			FurnaceModule.load();
 			LootTableModule.load();
-			skript.loadModules(new DamageSourceModule());
+			skript.loadModules(new DamageSourceModule(), new ItemComponentModule());
 		} catch (final Exception e) {
 			exception(e, "Could not load required .class files: " + e.getLocalizedMessage());
 			setEnabled(false);
