1dcbd3d679fa6f6bb30fa06ebd8adbabc37fcec3
Efnilite <35348263+Efnilite@users.noreply.github.com>
2025-10-24 07:05:59 +0200

    Fix function parameters with default value as list causing error (#8221)
    * Init commit
    
    * use Variable.SEPARATOR
    
    * requested changes

--- src/main/java/ch/njol/skript/lang/function/ScriptFunction.java
+++ src/main/java/ch/njol/skript/lang/function/ScriptFunction.java
@@ -13,6 +13,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.skriptlang.skript.lang.script.Script;
 
+import java.util.Arrays;
+
 public class ScriptFunction<T> extends Function<T> implements ReturnHandler<T> {
 
 	private final Trigger trigger;
@@ -60,11 +62,23 @@ public class ScriptFunction<T> extends Function<T> implements ReturnHandler<T> {
 			Parameter<?> parameter = parameters[i];
 			Object[] val = params[i];
 			if (parameter.single && val.length > 0) {
-				Variables.setVariable(parameter.name(), val[0], event, true);
-			} else {
+				Variables.setVariable(parameter.name, val[0], event, true);
+				continue;
+			}
+
+			boolean keyed = Arrays.stream(val).allMatch(it -> it instanceof KeyedValue<?>);
+			if (keyed) {
 				for (Object value : val) {
 					KeyedValue<?> keyedValue = (KeyedValue<?>) value;
-					Variables.setVariable(parameter.name() + "::" + keyedValue.key(), keyedValue.value(), event, true);
+					Variables.setVariable(parameter.name + Variable.SEPARATOR + keyedValue.key(), keyedValue.value(), event, true);
+				}
+			} else {
+				int count = 0;
+				for (Object value : val) {
+					// backup for if the passed argument is not a keyed value.
+					// an example of this is passing `xs: integers = (1, 2)` as a parameter.
+					Variables.setVariable(parameter.name + Variable.SEPARATOR + count, value, event, true);
+					count++;
 				}
 			}
 		}
